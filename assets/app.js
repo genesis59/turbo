de12307d064fbb7d0401b65885d177a3410e2edb
gregdev <gvandevray@numericable.fr>
2024-04-24 09:10:33 +0200

    fix bug modal bootstrap an sweetalert

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,25 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import { Modal } from 'bootstrap';
+document.addEventListener('turbo:before-cache', () => {
+    if (document.body.classList.contains('modal-open')) {
+        const modalEl = document.querySelector('.modal');
+        const modal = Modal.getInstance(modalEl);
+        modalEl.classList.remove('fade');
+        modal._backdrop._config.isAnimated = false;
+        modal.hide();
+        modal.dispose();
+    }
+    // internal way to see if sweetalert2 has been imported yet
+    if (__webpack_modules__[require.resolveWeak('sweetalert2')]) {
+        // because we know it's been imported, this will run synchronously
+        import('sweetalert2').then((Swal) => {
+            if (Swal.default.isVisible()) {
+                Swal.default.getPopup().style.animationDuration = '0ms'
+                Swal.default.close();
+            }
+        })
+    }
+});
